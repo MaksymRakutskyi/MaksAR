0d4699d3231a6ddbafcaeb7522288e8b1b68931c
MaksymRakutskyi <maksym.rakutskyi@gmail.com>
2021-10-05 18:40:57 +0300

    Add all my files

---
+++ WebDataBaseEDU/Data/UsersDB.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace WebDataBaseEDU.Data
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string Description { get; set; }
+        public bool IsActive { get; set; }
+        public string Role { get; set; }
+
+    }
+    
+
+}
